8c6e562c393b10b8fbbafafb28257c7ad9a15679
Matthias Huber <matthi.hub@gmail.com>
2025-01-05 19:54:21 +0100

    working deployment of one flask app

--- Pulumi/__main__.py
+++ Pulumi/__main__.py
@@ -1,32 +1,26 @@
-import pulumi
-from pulumi import Config, Output, ResourceOptions
-import pulumi_azure_native as azure_native
-from pulumi_azure_native import resources, network, web
-from pulumi_random import RandomString
-import pulumi_azure_native.consumption as consumption
+#CLCO Project
 from datetime import datetime
+
+import pulumi
+from pulumi import Config, Output
+from pulumi_azure_native import resources, network, cognitiveservices, web
+import pulumi_azure_native as azure_native
+from pulumi_random import random_string
+import pulumi_azure_native.consumption as consumption
 
 # Configuration variables
 config = Config()
 azure_location = config.get("azure-native:location") or "uksouth"
 
-# Repositories for the Flask web apps
-repo_urls = [
-    config.get("my:repoUrl1") or "https://github.com/huhubi/roulettenumbers",
-    config.get("my:repoUrl2") or "https://github.com/huhubi/roulettethirdsbet",
-    config.get("my:repoUrl3") or "https://github.com/huhubi/rouletteredblack"
-]
-branches = [
-    config.get("my:branch1") or "main",
-    config.get("my:branch2") or "main",
-    config.get("my:branch3") or "main"
-]
-
-start = datetime(2025, 1, 1).strftime('%Y-%m-%dT%H:%M:%SZ')
-end = datetime(2026, 2, 28).strftime('%Y-%m-%dT%H:%M:%SZ')
-mail_matthias = "wi22b112@technikum-wien.at"
-mail_gregoire = "wi22b060@technikum-wien.at"
+#defined_repo_url = config.get("my:repoUrl") or "https://github.com/huhubi/flaskwebapp"
+defined_repo_url = config.get("my:repoUrl") or "https://github.com/huhubi/roulettenumbers"
+defined_branch = config.get("my:branch") or "main"
+start = datetime(2025, 1, 1).strftime('%Y-%m-%dT%H:%M:%SZ')  # Startdatum: 1. November 2024
+end = datetime(2025, 2, 28).strftime('%Y-%m-%dT%H:%M:%SZ')
+mail_matthias ="wi22b112@technikum-wien.at"
+mail_gregoire ="wi22b060@technikum-wien.at"
 subscription_id = "5bb64e70-0225-40e2-b87c-ede62684f322"
+
 
 # Resource Group
 resource_group = resources.ResourceGroup('ProjectResourceGroup',
@@ -34,43 +28,46 @@ resource_group = resources.ResourceGroup('ProjectResourceGroup',
     location=azure_location
 )
 
-# Webapp Names
-webapp_names = ["flaskroulettewebapp1", "flaskroulettewebapp2", "flaskroulettewebapp3"]
-
-# Use random strings to give the web apps unique DNS names
-webapp_name_labels = [
-    RandomString(
-        f"{webapp_name}-",
-        length=8,
-        upper=False,
-        special=False,
-    ).result.apply(lambda result: f"{webapp_name}-{result}")
-    for webapp_name in webapp_names
-]
+# Use random strings to give the Webapp unique DNS names
+webapp_name_label1 = random_string.RandomString(
+    "flaskwebapp-",  # Prefix for the random string
+    length=8,  # Length of the random string
+    upper=False,  # Use lowercase letters
+    special=False,  # Do not use special characters
+).result.apply(lambda result: f"{web_app}-{result}")  # Format the result with the web app name
 
 # Virtual Network
 virtual_network = network.VirtualNetwork('virtualNetwork',
-    resource_group_name=resource_group.name,
-    location=azure_location,
+    resource_group_name=resource_group.name,  # Name of the resource group
+    location=azure_location,  # Location of the virtual network
     address_space=network.AddressSpaceArgs(
-        address_prefixes=['10.0.0.0/16']
+        address_prefixes=['10.0.0.0/16']  # Address space for the virtual network
     ),
-    virtual_network_name='ProjectVNet'
+    virtual_network_name='ProjectVNet'  # Name of the virtual network
 )
 
 # App Subnet
 app_subnet = network.Subnet('applicationSubnet',
-    resource_group_name=resource_group.name,
-    virtual_network_name=virtual_network.name,
-    subnet_name='applicationSubnet',
-    address_prefix='10.0.0.0/24',
+    resource_group_name=resource_group.name,  # Name of the resource group
+    virtual_network_name=virtual_network.name,  # Name of the virtual network
+    subnet_name='applicationSubnet',  # Name of the subnet
+    address_prefix='10.0.0.0/24',  # Address prefix for the subnet
     delegations=[
         network.DelegationArgs(
-            name='delegation',
-            service_name='Microsoft.Web/serverfarms',
+            name='delegation',  # Name of the delegation
+            service_name='Microsoft.Web/serverfarms',  # Service name for the delegation
         )
     ],
-    private_endpoint_network_policies='Enabled'
+    private_endpoint_network_policies='Enabled'  # Enable network policies for private endpoints
+)
+
+# Endpoint Subnet
+endpoint_subnet = network.Subnet('endpointSubnet',
+    resource_group_name=resource_group.name,  # Name of the resource group
+    virtual_network_name=virtual_network.name,  # Name of the virtual network
+    subnet_name='endpointSubnet',  # Name of the subnet
+    address_prefix='10.0.1.0/24',  # Address prefix for the subnet
+    private_endpoint_network_policies='Disabled'  # Disable network policies for private endpoints
 )
 
 # App Service Plan
@@ -81,153 +78,75 @@ app_service_plan = web.AppServicePlan('appServicePlan',
     sku=web.SkuDescriptionArgs(
         name='B1',
         tier='Basic',
-        capacity=3
+        capacity=3 # Set the capacity to 3
+        #Verify command: az webapp list-instances --resource-group ProjectResourceGroup --name Projectflaskwebapp
     ),
     kind='linux',
     reserved=True
 )
 
-######################################
-# Create Web Apps in sequence
-######################################
-
-# First Web App
-web_app1 = web.WebApp(
-    'webApp1',
-    resource_group_name=resource_group.name,
-    name=webapp_names[0],
-    location=azure_location,
-    server_farm_id=app_service_plan.id,
-    https_only=True,
-    kind='app,linux',
+# Web App
+web_app = web.WebApp('webApp',
+    resource_group_name=resource_group.name,  # Name of the resource group
+    name="Projectflaskwebapp",  # Name of the web app
+    location=azure_location,  # Location of the web app
+    server_farm_id=app_service_plan.id,  # ID of the associated App Service Plan
+    https_only=True,  # Enforce HTTPS-only traffic
+    kind='app,linux',  # Type of the web app
     site_config=web.SiteConfigArgs(
-        linux_fx_version='PYTHON|3.9',
-        app_settings=[
-            web.NameValuePairArgs(
-                name='WEBSITE_RUN_FROM_PACKAGE',
-                value='0'
-            ),
-        ],
-        always_on=True,
-        ftps_state='Disabled'
+        linux_fx_version='PYTHON|3.9',  # Runtime stack for the web app
+        always_on=True,  # Keep the web app always on
+        ftps_state='Disabled',  # Disable FTPS
+        app_command_line='pip install flask_socketio && FLASK_APP=app.py python -m flask run --host=0.0.0.0 --port=8000'
     )
 )
 
-# Second Web App - depends on the first Web App
-web_app2 = web.WebApp(
-    'webApp2',
+# VNet Integration
+vnet_integration = web.WebAppSwiftVirtualNetworkConnection('vnetIntegration',
+    name=web_app.name,
     resource_group_name=resource_group.name,
-    name=webapp_names[1],
-    location=azure_location,
-    server_farm_id=app_service_plan.id,
-    https_only=True,
-    kind='app,linux',
-    site_config=web.SiteConfigArgs(
-        linux_fx_version='PYTHON|3.9',
-        app_settings=[
-            web.NameValuePairArgs(
-                name='WEBSITE_RUN_FROM_PACKAGE',
-                value='0'
-            ),
-        ],
-        always_on=True,
-        ftps_state='Disabled'
-    ),
-    # This is the critical part to ensure the second WebApp waits for the first
-    opts=ResourceOptions(depends_on=[web_app1])
+    subnet_resource_id=app_subnet.id
 )
 
-# Third Web App (this one deploys in parallel by default,
-# but you could also specify depends_on=[web_app2] if you want it strictly sequential)
-web_app3 = web.WebApp(
-    'webApp3',
+source_control = azure_native.web.WebAppSourceControl("sourceControl",
+    name=web_app.name,
     resource_group_name=resource_group.name,
-    name=webapp_names[2],
-    location=azure_location,
-    server_farm_id=app_service_plan.id,
-    https_only=True,
-    kind='app,linux',
-    site_config=web.SiteConfigArgs(
-        linux_fx_version='PYTHON|3.9',
-        app_settings=[
-            web.NameValuePairArgs(
-                name='WEBSITE_RUN_FROM_PACKAGE',
-                value='0'
-            ),
-        ],
-        always_on=True,
-        ftps_state='Disabled'
-    ),
-    opts=ResourceOptions(depends_on=[web_app2])
+    repo_url=defined_repo_url,  # Replace with your repository URL
+    branch=defined_branch,  # Replace with your branch name
+    is_manual_integration=True,
+    deployment_rollback_enabled=False
 )
-
-# Collect them in a list for convenience
-web_apps = [web_app1, web_app2, web_app3]
-
-######################################
-# VNet Integration for all Web Apps
-######################################
-vnet_integrations = [
-    web.WebAppSwiftVirtualNetworkConnection(
-        f'vnetIntegration{i}',
-        name=web_apps[i].name,
-        resource_group_name=resource_group.name,
-        subnet_resource_id=app_subnet.id
-    )
-    for i in range(len(web_apps))
-]
-
-######################################
-# Source Controls for all Web Apps
-######################################
-source_controls = [
-    web.WebAppSourceControl(
-        f"sourceControl{i}",
-        name=web_apps[i].name,
-        resource_group_name=resource_group.name,
-        repo_url=repo_urls[i],
-        branch=branches[i],
-        is_manual_integration=True,
-        deployment_rollback_enabled=False
-    )
-    for i in range(len(web_apps))
-]
-
-######################################
-# Create a Budget for the Resource Group
-######################################
+# Create a budget for the specified resource group
 budget = resource_group.name.apply(lambda rg_name: consumption.Budget(
-    resource_name="Project-Budget",
-    scope=f"/subscriptions/{subscription_id}/resourceGroups/{rg_name}",
-    amount=50,
-    time_grain="Monthly",
-    time_period=consumption.BudgetTimePeriodArgs(
-        start_date=start,
-        end_date=end
-    ),
-    notifications={
-        "Actual2Percent": consumption.NotificationArgs(
-            enabled=True,
-            operator="GreaterThan",
-            threshold=2,
-            contact_emails=[mail_matthias, mail_gregoire],
-            contact_roles=[]
-        ),
-        "Actual50Percent": consumption.NotificationArgs(
-            enabled=True,
-            operator="GreaterThan",
-            threshold=50,
-            contact_emails=[mail_matthias, mail_gregoire],
-            contact_roles=[]
-        ),
+    resource_name="Project-Budget",  # Name of the budget resource
+    scope=f"/subscriptions/{subscription_id}/resourceGroups/{rg_name}",  # Scope limited to the resource group
+    amount=50,  # Budget amount
+    time_grain="Monthly",  # Budget reset interval
+    time_period={
+        "startDate": start,  # Start date of the budget
+        "endDate": end,  # End date of the budget
     },
-    category="Cost"
+    notifications={
+        "Actual2Percent": {  # Notification when 2% of the budget is reached
+            "enabled": True,
+            "operator": "GreaterThan",  # Condition: Budget exceeded
+            "threshold": 2,  # 2% of the budget
+            "contact_emails": [mail_matthias, mail_gregoire],  # Email addresses for notifications
+            "contact_roles": [],  # No specific roles
+            "notification_language": "en-US",  # Notification language
+        },
+        "Actual50Percent": {  # Notification when 50% of the budget is reached
+            "enabled": True,
+            "operator": "GreaterThan",
+            "threshold": 50,  # 50% of the budget
+            "contact_emails": [mail_matthias, mail_gregoire],
+            "contact_roles": [],
+            "notification_language": "en-US",
+        },
+    },
+    category="Cost",  # Budget category for cost monitoring
 ))
 
-######################################
-# Export the Web App hostnames
-######################################
-for i, web_app in enumerate(web_apps):
-    pulumi.export(f"hostname_{i+1}", 
-        Output.concat(f"[Web App {i+1}](http://", web_app.default_host_name, ")")
-    )
+
+# Export the Web App hostname as a Markdown link
+pulumi.export("hostname", pulumi.Output.concat("[Web App](http://", web_app.default_host_name, ")"))
